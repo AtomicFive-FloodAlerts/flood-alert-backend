043c463ae902d0c5966752ec0f46b239b7c6f26c
shaeshanth2004 <227603496+shaeshanth2004@users.noreply.github.com>
2026-03-15 12:20:16 +0530

    Initial commit

---
+++ src/test/java/Atomic5/demo/FloodAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package Atomic5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
